d6e402bcbaf3e402a1430bb252cf0d56cf0a049f
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-12-07 22:55:02 +0100

    Switch to API data

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import { categories } from "../constants";
 import fetchNews from "../utils/fetchNews";
 import NewsList from "./NewsList";
-import response from "../response.json";
+//import response from "../response.json";
 
 async function Homepage() {
   //fetch the news data
 
   const news: NewsResponse =
-    response || (await fetchNews(categories.join(",")));
-  // await fetchNews(categories.join(","));
+    //response || (await fetchNews(categories.join(",")));
+    await fetchNews(categories.join(","));
   //console.log(news, "news");
   return (
     <div>
